dd4fae459d5e6886f60a4d39533f41f7c6460868
NguyenTruongVu <nguyentruongvu000111@gmail.com>
2026-08-31 09:20:59 +0700

    Cấu hình xử lí request cho các thiết bị kết nối cùng mạng với Server

--- functions/index.js
+++ functions/index.js
@@ -1,32 +1,21 @@
 /**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
+ * Local network access setup:
+ * firebase emulators:start --only functions --host 0.0.0.0
+ * then open http://<your-lan-ip>:5001/lexifold-d8b1c/us-central1/helloWorld
  */
 
-const {setGlobalOptions} = require("firebase-functions");
-const {onRequest} = require("firebase-functions/https");
-const logger = require("firebase-functions/logger");
+const functions = require("firebase-functions");
 
-// For cost control, you can set the maximum number of containers that can be
-// running at the same time. This helps mitigate the impact of unexpected
-// traffic spikes by instead downgrading performance. This limit is a
-// per-function limit. You can override the limit for each function using the
-// `maxInstances` option in the function's options, e.g.
-// `onRequest({ maxInstances: 5 }, (req, res) => { ... })`.
-// NOTE: setGlobalOptions does not apply to functions using the v1 API. V1
-// functions should each use functions.runWith({ maxInstances: 10 }) instead.
-// In the v1 API, each function can only serve one request per container, so
-// this will be the maximum concurrent request count.
-setGlobalOptions({ maxInstances: 10 });
+exports.helloWorld = functions.https.onRequest((req, res) => {
+  const clientIp = req.headers["x-forwarded-for"] || req.socket.remoteAddress || "unknown";
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
+  functions.logger.info("helloWorld called", { clientIp });
 
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+  res.status(200).json({
+    ok: true,
+    message: "Hello from Firebase Functions on LAN",
+    clientIp,
+    timestamp: new Date().toISOString(),
+    path: req.path,
+  });
+});
